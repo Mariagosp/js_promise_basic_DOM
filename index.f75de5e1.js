6918b0cff5ab83e6f2e7cb55cff1e1ef66e9217a
Maria <m.gosp1704@gmail.com>
2024-12-13 16:17:13 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,38 @@
+"use strict";
+const promise1 = new Promise((resolve, reject)=>{
+    const logo = document.querySelector(".logo");
+    logo.addEventListener("click", ()=>{
+        resolve("Promise was resolved!");
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject(new Error("Promise was rejected!"));
+    }, 3000);
+});
+promise1.then((message)=>{
+    const successMessage = document.createElement("div");
+    successMessage.classList.add("message");
+    successMessage.textContent = "Promise was resolved!";
+    document.body.appendChild(successMessage);
+}).catch(()=>{
+    const errorMessage = document.createElement("div");
+    errorMessage.classList.add("message");
+    errorMessage.classList.add("error-message");
+    errorMessage.textContent = "Promise was rejected!";
+    document.body.appendChild(errorMessage);
+});
+promise2.then((message)=>{
+    const successMessage = document.createElement("div");
+    successMessage.classList.add("message");
+    successMessage.textContent = "Promise was resolved!";
+    document.body.appendChild(successMessage);
+}).catch(()=>{
+    const errorMessage = document.createElement("div");
+    errorMessage.classList.add("message");
+    errorMessage.classList.add("error-message");
+    errorMessage.textContent = "Promise was rejected!";
+    document.body.appendChild(errorMessage);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
